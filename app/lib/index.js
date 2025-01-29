ae3c9734972c324441a9812167c45fbb34691292
Шерстобоев Илья <sherstoboevilya@gmail.com>
2025-01-29 23:25:08 +0500

    init

---
+++ app/lib/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const reference_1 = require("../../reference");
+(0, reference_1.helloWorld)();
---
+++ app/src/index.ts
@@ -0,0 +1,3 @@
+import { helloWorld } from '../../reference';
+
+helloWorld();
